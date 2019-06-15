276c6eb1d7580734a4b62f2ee10078bf28a8a11f
zHorical <50248672+zHorical@users.noreply.github.com>
2019-06-16 01:43:15 +0300

    Add files via upload

---
+++ Broadcast.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+  const bot = new Discord.Client();
+      bot.on('ready', () => {
+        bot.user.setGame(`Arab Village Bot`,'https://www.twitch.tv/ArabGN');
+          console.log('Im Ready!');
+  
+        });
+
+  bot.on('message', message => {
+    if (message.content.split(' ')[0] == '/b')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   bot.on("message", message => {
+       var prefix = "$";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+bot.login(process.env.BOT_TOKEN);
